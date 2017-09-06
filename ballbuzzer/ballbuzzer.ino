d556b3c1c8584c66d992a91b28187768e3bcb7e8
John Nemec <johnnemec@gmail.com>
2017-09-05 22:40:54 -0500

    added delaytime variable for quick delay updates

--- ballbuzzer/ballbuzzer.ino
+++ ballbuzzer/ballbuzzer.ino
@@ -2,6 +2,7 @@ int tiltball = 2; // the pin of the Tilt Ball Switch
 // for late night work, replace the Active Buzzer with an LED to not wake up the wife :)
 // int buzzer = 13; // Quiet Buzzer - you can replace with the onboard LED
 int buzzer = 12; // the pin of the Active Buzzer
+int delaytime = 2; // the delay time
 
 void setup() {
   pinMode(tiltball,INPUT); // initialize the Tilt Ball Switch as an input
@@ -18,8 +19,8 @@ void loop() {
     digitalWrite(buzzer,LOW); // turn Active Buzzer off
   } else {
     digitalWrite(buzzer,HIGH); // turn Active Buzzer on
-    delay(2); // wait for 1ms
+    delay(delaytime); // wait for 1ms
     digitalWrite(buzzer,LOW); // turn Active Buzzer off
-    delay(2); // wait for 1ms
+    delay(delaytime); // wait for 1ms
   }
 }
