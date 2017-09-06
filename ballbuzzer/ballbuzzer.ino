3b2ac2dbef7d6182926c3a68c0382cca834d546f
John Nemec <Moosefish@moosefishs-mbp.home>
2017-09-05 22:37:57 -0500

    Tilt Ball Switch and Active Buzzer, with fallback LED option.

--- ballbuzzer/ballbuzzer.ino
+++ ballbuzzer/ballbuzzer.ino
@@ -1,34 +1,25 @@
-//www.elegoo.com
-//2016.12.08
-/*****************************************/
-const int ledPin = 13;//the led attach to
-int buzzer = 12; //the pin of the active buzzer
+int tiltball = 2; // the pin of the Tilt Ball Switch
+// for late night work, replace the Active Buzzer with an LED to not wake up the wife :)
+// int buzzer = 13; // Quiet Buzzer - you can replace with the onboard LED
+int buzzer = 12; // the pin of the Active Buzzer
 
-void setup()
-{ 
-  pinMode(ledPin,OUTPUT);//initialize the ledPin as an output
-  pinMode(2,INPUT);
-  pinMode(buzzer,OUTPUT);//initialize the buzzer pin as an output
-  digitalWrite(2, HIGH);
-} 
-/******************************************/
-void loop() 
-{  
-  int digitalVal = digitalRead(2);
-  if(HIGH == digitalVal)
-  {
-    digitalWrite(ledPin,LOW);//turn the led off
-    digitalWrite(buzzer,LOW);
-  }
-  else
-  {
-    digitalWrite(ledPin,HIGH);//turn the led on 
-    
-    digitalWrite(buzzer,HIGH);
-    delay(2);//wait for 1ms
-    digitalWrite(buzzer,LOW);
-    delay(2);//wait for 1ms
+void setup() {
+  pinMode(tiltball,INPUT); // initialize the Tilt Ball Switch as an input
+  pinMode(buzzer,OUTPUT); // initialize the Active Buzzer pin as an output
+
+  // This confuses me, I'm assuming HIGH means connected but in practice it is not
+  digitalWrite(tiltball, HIGH); // turn the tiltball on
+}
+
+void loop() {
+  int digitalVal = digitalRead(tiltball);
+
+  if(HIGH == digitalVal) { // so when the tiltball is pointed down turn off the buzzer. But HIGH to me means on, but in truth it is off
+    digitalWrite(buzzer,LOW); // turn Active Buzzer off
+  } else {
+    digitalWrite(buzzer,HIGH); // turn Active Buzzer on
+    delay(2); // wait for 1ms
+    digitalWrite(buzzer,LOW); // turn Active Buzzer off
+    delay(2); // wait for 1ms
   }
 }
-/**********************************************/
-
